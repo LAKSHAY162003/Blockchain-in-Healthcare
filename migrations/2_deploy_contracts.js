df8230a76f20d72f686a62c3f941d07e5c7e2556
LAKSHAY162003 <lakshay.sharma.ug21@nsut.ac.in>
2024-10-22 12:04:47 +0530

    minor dev bugs

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,7 +5,7 @@ module.exports = async function(deployer) {
     // Deploy the Oracle contract first
     await deployer.deploy(Oracle);
     const oracleInstance = await Oracle.deployed(); // Get the deployed instance
-
+    console.log("This is address : "+oracleInstance.address)
     // Now deploy the HealthcareAutomationWithIPFS contract with the address of the Oracle
     await deployer.deploy(HealthcareAutomationWithIPFS, oracleInstance.address);
 };
